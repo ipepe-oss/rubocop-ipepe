e98658424f970e79de113924a51d28bb6945a4f3
Patryk Ptasinski <patryk@ipepe.pl>
2023-09-16 22:50:23 +0200

    Initial bundle gem

---
+++ lib/rubocop/ipepe.rb
@@ -0,0 +1,8 @@
+require "rubocop/ipepe/version"
+
+module Rubocop
+  module Ipepe
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rubocop/ipepe/version.rb
@@ -0,0 +1,5 @@
+module Rubocop
+  module Ipepe
+    VERSION = "0.1.0"
+  end
+end
