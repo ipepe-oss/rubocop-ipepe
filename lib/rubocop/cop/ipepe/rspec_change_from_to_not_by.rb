d2bb2ca2b3920c89aba9825966d4759c3d9ded10
Patryk Ptasiński <github@ipepe.pl>
2025-06-05 15:59:32 +0200

    Add RspecChangeFromToNotBy cop

---
+++ lib/rubocop/cop/ipepe/rspec_change_from_to_not_by.rb
@@ -0,0 +1,25 @@
+module RuboCop
+  module Cop
+    module Ipepe
+      class RspecChangeFromToNotBy < ::RuboCop::Cop::Base
+        MSG = 'Prefer `change { }.from().to()` over `change { }.by()`'.freeze
+
+        def on_send(node)
+          return unless change_by?(node)
+
+          add_offense(node)
+        end
+
+        private
+
+        def change_by?(node)
+          return false unless node.method?(:by)
+
+          receiver = node.receiver
+          receiver = receiver.send_node if receiver&.block_type?
+          receiver&.send_type? && receiver.method?(:change)
+        end
+      end
+    end
+  end
+end
