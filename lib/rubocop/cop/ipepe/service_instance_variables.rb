b4defd9065332e75ebae5881a383d3d680e9cd8e
Patryk Ptasiński <github@ipepe.pl>
2025-06-10 14:19:07 +0200

    Make service instance variables path configurable

--- lib/rubocop/cop/ipepe/service_instance_variables.rb
+++ lib/rubocop/cop/ipepe/service_instance_variables.rb
@@ -3,6 +3,7 @@ module RuboCop
     module Ipepe
       class ServiceInstanceVariables < ::RuboCop::Cop::Base
         MSG = "Do not use instance variables outside #initialize and #call".freeze
+        DEFAULT_PATHS = ["app/services/**/*.rb"].freeze
 
         def on_ivar(node)
           check_node(node)
@@ -15,7 +16,7 @@ module RuboCop
         private
 
         def check_node(node)
-          return unless app_service_file?
+          return unless included_file?
 
           method = node.each_ancestor(:def).first
           return if method && [:initialize, :call].include?(method.method_name)
@@ -23,8 +24,11 @@ module RuboCop
           add_offense(node)
         end
 
-        def app_service_file?
-          processed_source.file_path.include?(File.join("app", "services"))
+        def included_file?
+          Array(cop_config["Include"] || DEFAULT_PATHS).any? do |pattern|
+            File.fnmatch?(pattern, processed_source.file_path,
+                          File::FNM_PATHNAME | File::FNM_EXTGLOB)
+          end
         end
       end
     end
--- spec/rubocop/cop/ipepe/service_instance_variables_spec.rb
+++ spec/rubocop/cop/ipepe/service_instance_variables_spec.rb
@@ -1,12 +1,15 @@
 require "spec_helper"
 
 RSpec.describe RuboCop::Cop::Ipepe::ServiceInstanceVariables do
-  def config
-    RuboCop::Config.new("AllCops" => { "DisplayCopNames" => true })
+  def config(include_path = "app/services/**/*.rb")
+    RuboCop::Config.new(
+      "AllCops" => { "DisplayCopNames" => true },
+      "Ipepe/ServiceInstanceVariables" => { "Include" => [include_path] }
+    )
   end
 
-  def cop
-    described_class.new(config)
+  def cop(include_path = "app/services/**/*.rb")
+    @cop ||= described_class.new(config(include_path))
   end
 
   it "registers an offense for instance variable outside initialize and call" do
@@ -37,6 +40,28 @@ RSpec.describe RuboCop::Cop::Ipepe::ServiceInstanceVariables do
 
         def call
           @foo
+        end
+      end
+    RUBY
+  end
+
+  it "does not register an offense when file is outside included paths" do
+    expect_no_offenses(<<~RUBY, "app/models/test_service.rb")
+      class TestService
+        def other
+          @foo = 1
+        end
+      end
+    RUBY
+  end
+
+  it "allows configuring additional paths" do
+    @cop = described_class.new(config("app/models/**/*.rb"))
+    expect_offense(<<~RUBY, "app/models/test_service.rb")
+      class TestService
+        def other
+          @foo
+          ^^^^ Ipepe/ServiceInstanceVariables: Do not use instance variables outside #initialize and #call
         end
       end
     RUBY
